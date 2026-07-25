e62874417d87cbd8aefe498c21ce1a01b938d31d
brnagn7 <brnagn7@gmail.com>
2026-07-25 14:37:18 -0400

    Arrange dashboard cards using GridPane

--- src/main/java/net/edmooney/netwatch/view/DashboardPane.java
+++ src/main/java/net/edmooney/netwatch/view/DashboardPane.java
@@ -1,6 +1,7 @@
 package net.edmooney.netwatch.view;
 
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import net.edmooney.netwatch.view.components.DashboardCard;
 
@@ -15,13 +16,20 @@ public class DashboardPane {
         Label title = new Label("Dashboard");
         title.getStyleClass().add("dashboard-title");
 
+        GridPane grid = new GridPane();
+
+        grid.setHgap(20);
+        grid.setVgap(20);
+
         DashboardCard cardFactory = new DashboardCard();
 
-        dashboard.getChildren().addAll(
-                title,
-                cardFactory.createCard("Upload", "0 Mbps"),
-                cardFactory.createCard("Download", "0 Mbps")
-        );
+        grid.add(cardFactory.createCard("Upload", "0 Mbps"), 0, 0);
+        grid.add(cardFactory.createCard("Download", "0 Mbps"), 1, 0);
+
+        grid.add(cardFactory.createCard("Connected Hosts", "0"), 0, 1);
+        grid.add(cardFactory.createCard("Active Ports", "0"), 1, 1);
+
+        dashboard.getChildren().addAll(title, grid);
 
         return dashboard;
     }
