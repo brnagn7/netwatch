c4ce09914b18b0fc46d949a19095fc4c6b1e9ac3
brnagn7 <brnagn7@gmail.com>
2026-07-31 16:10:17 -0400

    Store previous network counters

--- src/main/java/net/edmooney/netwatch/platform/WindowsTrafficProvider.java
+++ src/main/java/net/edmooney/netwatch/platform/WindowsTrafficProvider.java
@@ -2,20 +2,48 @@ package net.edmooney.netwatch.platform;
 
 import net.edmooney.netwatch.model.NetworkAdapter;
 import net.edmooney.netwatch.model.TrafficSnapshot;
+import oshi.SystemInfo;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.hardware.NetworkIF;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class WindowsTrafficProvider implements TrafficProvider {
+
+    private final SystemInfo systemInfo = new SystemInfo();
+    private final HardwareAbstractionLayer hardware = systemInfo.getHardware();
+    private long previousBytesReceived = -1;
+    private long previousBytesSent = -1;
 
     @Override
     public TrafficSnapshot collectSnapshot(NetworkAdapter adapter) {
 
-        int[] count = new int[1];
+        NetworkIF selectedInterface = null;
 
-        int result = WindowsNetworkApi.INSTANCE.GetNumberOfInterfaces(count);
+        List<NetworkIF> interfaces = hardware.getNetworkIFs();
 
-        System.out.println("Result: " + result);
-        System.out.println("Interfaces: " + count[0]);
+        for (NetworkIF networkIF : interfaces) {
+
+            networkIF.updateAttributes();
+
+            if (networkIF.getDisplayName().equals(adapter.getDisplayName())) {
+                selectedInterface = networkIF;
+                break;
+            }
+        }
+
+        if (selectedInterface != null) {
+
+            System.out.println("-------------------------");
+            System.out.println("Monitoring: " + selectedInterface.getDisplayName());
+            System.out.println("Bytes Received: " + selectedInterface.getBytesRecv());
+            System.out.println("Bytes Sent: " + selectedInterface.getBytesSent());
+
+        } else {
+
+            System.out.println("Adapter not found: " + adapter.getDisplayName());
+        }
 
         return new TrafficSnapshot(
                 LocalDateTime.now(),
