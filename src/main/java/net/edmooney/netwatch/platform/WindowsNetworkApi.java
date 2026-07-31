392f8a14d80b09530c07d0d3f72776ab3a7e70a1
brnagn7 <brnagn7@gmail.com>
2026-07-31 15:50:24 -0400

    Add WindowsNetworkApi interface

---
+++ src/main/java/net/edmooney/netwatch/platform/WindowsNetworkApi.java
@@ -0,0 +1,11 @@
+package net.edmooney.netwatch.platform;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+public interface WindowsNetworkApi extends Library {
+
+    WindowsNetworkApi INSTANCE =
+            Native.load("Iphlpapi", WindowsNetworkApi.class);
+
+}
