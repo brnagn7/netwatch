34956dc7814f632bf4339b7df7c2c16abc7338c5
brnagn7 <brnagn7@gmail.com>
2026-08-01 11:32:35 -0400

    Display hosts using TableView

--- src/main/java/net/edmooney/netwatch/view/HostsView.java
+++ src/main/java/net/edmooney/netwatch/view/HostsView.java
@@ -2,7 +2,9 @@ package net.edmooney.netwatch.view;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import net.edmooney.netwatch.model.Host;
 import net.edmooney.netwatch.service.HostDiscoveryService;
@@ -16,22 +18,31 @@ public class HostsView extends BorderPane {
         Label title = new Label("Connected Hosts");
         title.getStyleClass().add("dashboard-title");
 
-        ListView<String> hostList = new ListView<>();
+        TableView<Host> table = new TableView<>();
+
+        TableColumn<Host, String> ipColumn = new TableColumn<>("IP Address");
+        ipColumn.setCellValueFactory(new PropertyValueFactory<>("ipAddress"));
+        ipColumn.setPrefWidth(180);
+
+        TableColumn<Host, String> hostColumn = new TableColumn<>("Host Name");
+        hostColumn.setCellValueFactory(new PropertyValueFactory<>("hostName"));
+        hostColumn.setPrefWidth(250);
+
+        TableColumn<Host, Boolean> statusColumn = new TableColumn<>("Online");
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("online"));
+        statusColumn.setPrefWidth(100);
+
+        table.getColumns().addAll(
+                ipColumn,
+                hostColumn,
+                statusColumn
+        );
 
         HostDiscoveryService service = new HostDiscoveryService();
 
-        for (Host host : service.discoverHosts()) {
-
-            hostList.getItems().add(
-                    host.getIpAddress()
-                            + "    "
-                            + host.getHostName()
-                            + "    "
-                            + (host.isOnline() ? "Online" : "Offline")
-            );
-        }
+        table.getItems().addAll(service.discoverHosts());
 
         setTop(title);
-        setCenter(hostList);
+        setCenter(table);
     }
 }
